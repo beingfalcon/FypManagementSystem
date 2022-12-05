27b2afdf444ff0e133205e1ee467ad36442ab2b0
Rameez Ahmed <rameez98ahmed@gmail.com>
2022-12-05 22:14:15 +0500

    Pooling

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     host: 'sql9.freesqldatabase.com',
     dialect: 'mysql',
     pool: {
-      max: 5,
+      max: 1000,
       min: 0,
       acquire: 30000,
       idle: 10000
