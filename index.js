9598dd8ed986fe926b9728603089ba89145b5380
Rameez Ahmed <rameez98ahmed@gmail.com>
2022-12-06 15:02:58 +0500

    Added Path to Views

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('view engine','ejs');
 app.use(expressLayouts);
 app.use(express.static (path.join(__dirname,"./public")));
 app.use(express.static (path.join(__dirname,"./uploads")));
+app.use(express.static (path.join(__dirname,"./views")));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
